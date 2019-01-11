158d420a8f6e55ec743943d053703e02b842a913
EvgenyKashin <kashinevge@gmail.com>
2019-01-11 11:37:36 +0300

    Add splitting to folds

---
+++ make_folds.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+import json
+from itertools import product
+from sklearn.model_selection import StratifiedKFold
+
+base_path = Path('/mnt/ssd0_1/kashin/ai_edge/segmentation/')
+train_ann_path = base_path / 'seg_train_annotations'
+
+routes = []
+tods = []
+names = []
+
+for ann in train_ann_path.iterdir():
+    if '.json' == ann.suffix:
+        names.append(ann.name.split('.')[0])
+        meta = json.loads(ann.read_text())
+        routes.append(meta['attributes']['route'])
+        tods.append(meta['attributes']['timeofday'])
+
+attr_prod = list(product(set(routes), set(tods)))
+attr_prod_ind = []
+
+for route, tod in zip(routes, tods):
+    index = attr_prod.index((route, tod))
+    attr_prod_ind.append(index)
+
+folds = {}
+skf = StratifiedKFold(n_splits=5, random_state=42)
+splits = skf.split(attr_prod_ind, attr_prod_ind)
+
+for i, split in enumerate(splits):
+    fold_names = []
+    for j in split[1]:
+        fold_names.append(names[j])
+    folds[i] = fold_names
+
+with open('folds_split.json', 'w') as f:
+    json.dump(folds, f)
