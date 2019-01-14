3f311129f59f3d4011f0128a3dc7a14b1fda228f
EvgenyKashin <kashinevge@gmail.com>
2019-01-15 00:57:12 +0300

    Update training

--- train.py
+++ train.py
@@ -1,14 +1,16 @@
 import json
 from pathlib import Path
 import argparse
-import numpy as np
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
 import torch.backends.cudnn as cudnn
+from torch.optim import Adam
 from dataset import SignateSegDataset
 from models import ResNetUnet
 from losses import CrossEntropyLoss2d
+import utils
+from validation import validation
 
 from albumentations.torch import ToTensor
 from albumentations import (
@@ -19,6 +21,7 @@ from albumentations import (
     PadIfNeeded,
     RandomCrop,
     CenterCrop,
+    Resize
 )
 
 base_path = Path('/mnt/ssd0_1/kashin/ai_edge/segmentation')
@@ -29,17 +32,21 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--backbone', default='resnet34', type=str)
     parser.add_argument('--is_deconv', default=False, type=lambda x: str(x).lower() == 'true')
-    parser.add_argument('--device_ids', default='0', type=str)
-    parser.add_argument('--root', default='runs/debug', type=str)
-    parser.add_argument('--train_crop_width', default=512, type=int)
-    parser.add_argument('--train_crop_height', default=512, type=int)
-    parser.add_argument('--val_crop_width', default=512, type=int)
-    parser.add_argument('--val_crop_height', default=512, type=int)
+    parser.add_argument('--device_ids', default='0,1', type=str)
+    parser.add_argument('--root',
+                        default='/mnt/ssd0_1/kashin/ai_edge/segmentation/runs/resize_960',
+                        type=str)
+    parser.add_argument('--train_crop_width', default=1024, type=int)
+    parser.add_argument('--train_crop_height', default=1024, type=int)
+    parser.add_argument('--train_resize_width', default=960, type=int)
+    parser.add_argument('--train_resize_height', default=576, type=int)
+    parser.add_argument('--val_crop_width', default=1024, type=int)
+    parser.add_argument('--val_crop_height', default=1024, type=int)
     parser.add_argument('--num_workers', default=10, type=int)
     parser.add_argument('--fold', default=0, type=int)
-    parser.add_argument('--batch_size', default=8, type=int)
+    parser.add_argument('--batch_size', default=5, type=int)
     parser.add_argument('--lr', default=0.0001, type=float)
-    parser.add_argument('--n_epochs', default=100, type=int)
+    parser.add_argument('--n_epochs', default=200, type=int)
 
     args = parser.parse_args()
 
@@ -73,16 +80,18 @@ def main():
 
     def train_transform(p=1):
         return Compose([
-            PadIfNeeded(args.train_crop_height, args.train_crop_width),
-            RandomCrop(args.train_crop_height, args.train_crop_width),
+            Resize(args.train_resize_height, args.train_resize_width),
+            # PadIfNeeded(args.train_crop_height, args.train_crop_width),
+            # RandomCrop(args.train_crop_height, args.train_crop_width),
             Normalize(),
             ToTensor(num_classes=num_classes)
         ], p=1)
 
     def val_transform(p=1):
         return Compose([
-            PadIfNeeded(args.train_crop_height, args.train_crop_width),
-            CenterCrop(args.train_crop_height, args.train_crop_width),
+            Resize(args.train_resize_height, args.train_resize_width),
+            # PadIfNeeded(args.train_crop_height, args.train_crop_width),
+            # CenterCrop(args.train_crop_height, args.train_crop_width),
             Normalize(),
             ToTensor(num_classes=num_classes)
         ])
@@ -99,11 +108,22 @@ def main():
 
     train_loader = make_loader(train_file_names, shuffle=True, transform=train_transform(),
                                batch_size=args.batch_size)
-    valid_loader = make_loader(val_file_names, transform=val_transform(),
-                               batch_size=len(device_ids))
+    val_loader = make_loader(val_file_names, transform=val_transform(),
+                             batch_size=len(device_ids))
 
     root.joinpath('params.json').write_text(
         json.dumps(vars(args), indent=True, sort_keys=True)
+    )
+
+    utils.train(
+        args=args,
+        model=model,
+        criterion=loss,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        validation=validation,
+        init_optimizer=lambda lr: Adam(model.parameters(), lr),
+        num_classes=num_classes
     )
 
 
---
+++ utils.py
@@ -0,0 +1,116 @@
+from pathlib import Path
+import random
+import json
+from datetime import datetime
+import numpy as np
+import torch
+from torch import nn
+from tqdm import tqdm
+
+
+def cuda(x):
+    return x.cuda(async=True) if torch.cuda.is_available() else x
+
+
+def write_event(log, step, **data):
+    data['step'] = step
+    data['dt'] = datetime.now().isoformat()
+    log.write(json.dumps(data, sort_keys=True))
+    log.write('\n')
+    log.flush()
+
+
+def train(args, model: nn.Module, criterion, train_loader, val_loader, validation,
+          init_optimizer, num_classes=None):
+    lr = args.lr
+    n_epochs = args.n_epochs
+    fold = args.fold
+    optimizer = init_optimizer(lr)
+
+    root = Path(args.root)
+    model_path = root / f'model_{fold}.pth'
+    model_path_best = root / f'model_{fold}_best.pth'
+    if model_path.exists():
+        state = torch.load(str(model_path))
+        epoch = state['epoch']
+        step = state['step']
+        model.load_state_dict(state['model'])
+        print(f'Restored model, epoch {epoch}, step {step}')
+    else:
+        epoch = 1
+        step = 0
+
+    save = lambda ep: torch.save({
+        'model': model.state_dict(),
+        'epoch': ep,
+        'step': step
+    }, str(model_path))
+
+    save_best = lambda ep: torch.save({
+        'model': model.state_dict(),
+        'epoch': ep,
+        'step': step
+    }, str(model_path_best))
+
+    report_each = 10
+    log = root.joinpath(f'train_{fold}.log').open('a')
+    valid_losses = []
+    min_loss = 1e6
+    best_epoch = 0
+    for epoch in range(epoch, n_epochs + 1):
+        model.train()
+        random.seed()
+        tq = tqdm(total=len(train_loader) * args.batch_size)
+        tq.set_description(f'Epoch: {epoch}, lr: {lr}')
+        losses = []
+
+        try:
+            mean_loss = 0
+            for i, (inputs, targets) in enumerate(train_loader):
+                inputs = cuda(inputs)
+
+                with torch.no_grad():
+                    targets = cuda(targets)
+
+                optimizer.zero_grad()
+                outputs = model(inputs)
+                loss = criterion(outputs, targets)
+                batch_size = inputs.size(0)
+                loss.backward()
+                optimizer.step()
+
+                step += 1
+                tq.update(batch_size)
+                losses.append(loss.item())
+                mean_loss = np.mean(losses[-report_each:])
+                tq.set_postfix(loss=f'{mean_loss:.5f}')
+                if i and i % report_each == 0:
+                    write_event(log, step, loss=mean_loss)
+
+            write_event(log, step, loss=mean_loss)
+            tq.close()
+            save(epoch + 1)
+            valid_metrics = validation(model, criterion, val_loader, num_classes)
+            write_event(log, step, **valid_metrics)
+            valid_loss = valid_metrics['valid_loss']
+            valid_losses.append(valid_loss)
+
+            if valid_loss < min_loss:
+                min_loss = valid_loss
+                save_best(epoch)
+                write_event(log, step, best_model=True)
+                print('New best model')
+                best_epoch = epoch
+
+            if epoch - best_epoch > 10:
+                write_event(log, step, early_stopping=True)
+                print('Early stopping')
+                return
+            # TODO: add to tqdm valid loss
+        except KeyboardInterrupt:
+            tq.close()
+            print('Ctrl+C, saving snapshot')
+            save(epoch)
+            print('done.')
+            return
+
