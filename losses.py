afbf64dd546405450e323c1edc466791e25c1786
EvgenyKashin <kashinevge@gmail.com>
2019-02-02 19:56:25 +0300

    Clear formatting

--- losses.py
+++ losses.py
@@ -28,33 +28,32 @@ class LossMulti(nn.Module):
 
 
 class FocalLoss(nn.Module):
+    # Adopted from: https://github.com/clcarwin/focal_loss_pytorch/blob/master/focalloss.py
     def __init__(self, gamma=0, alpha=None, size_average=True):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
         self.alpha = alpha
-        # if isinstance(alpha,(float,int,long)): self.alpha = torch.Tensor([alpha,1-alpha])
-        # if isinstance(alpha,list): self.alpha = torch.Tensor(alpha)
         self.size_average = size_average
 
-    def forward(self, input, target):
-        if input.dim()>2:
-            input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
-            input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
-            input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
-        target = target.view(-1,1)
+    def forward(self, inputs, targets):
+        if inputs.dim() > 2:
+            inputs = inputs.view(inputs.size(0), inputs.size(1),-1)  # N,C,H,W => N,C,H*W
+            inputs = inputs.transpose(1, 2)  # N,C,H*W => N,H*W,C
+            inputs = inputs.contiguous().view(-1, inputs.size(2))   # N,H*W,C => N*H*W,C
+        targets = targets.view(-1, 1)
 
-        logpt = F.log_softmax(input)
-        logpt = logpt.gather(1,target)
+        logpt = F.log_softmax(inputs)
+        logpt = logpt.gather(1, targets)
         logpt = logpt.view(-1)
         pt = Variable(logpt.data.exp())
 
         if self.alpha is not None:
-            if self.alpha.type()!=input.data.type():
-                self.alpha = self.alpha.type_as(input.data)
-            at = self.alpha.gather(0,target.data.view(-1))
+            if self.alpha.type() != inputs.data.type():
+                self.alpha = self.alpha.type_as(inputs.data)
+            at = self.alpha.gather(0, targets.data.view(-1))
             logpt = logpt * Variable(at)
 
-        loss = -1 * (1-pt)**self.gamma * logpt
+        loss = -1 * (1 - pt) ** self.gamma * logpt
         if self.size_average:
             return loss.mean()
         else:
